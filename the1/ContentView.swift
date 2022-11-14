ef7fa8d0e1bdb2468cd8ff39756947575949fbdb
Amani <amalmutairi22@twq.idserve.net>
2022-11-14 13:51:09 +0300

    Initial Commit

---
+++ the1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  the1
+//
+//  Created by Amani Almutairi  on 20/04/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ the1/the1App.swift
@@ -0,0 +1,17 @@
+//
+//  the1App.swift
+//  the1
+//
+//  Created by Amani Almutairi  on 20/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct the1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
